16369862b298e3645acf13f96387323feb15afb9
Jaeeon Lee <dannylee88@naver.com>
2023-06-24 18:22:25 +0900

    register

--- src/main/java/com/example/booksearch/controller/BookSearchController.java
+++ src/main/java/com/example/booksearch/controller/BookSearchController.java
@@ -1,5 +1,6 @@
 package com.example.booksearch.controller;
 
+import com.example.booksearch.dto.BookSearchDTO;
 import com.example.booksearch.dto.PageRequestDTO;
 import com.example.booksearch.service.BookSearchService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/booksearch")
@@ -25,5 +28,20 @@ public class BookSearchController {
     @GetMapping("list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("register")
+    public void register() {
+
+    }
+
+    @PostMapping("register")
+    public String registerPost(BookSearchDTO dto, RedirectAttributes redirectAttributes){
+
+        Long bno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/booksearch/list";
     }
 }
