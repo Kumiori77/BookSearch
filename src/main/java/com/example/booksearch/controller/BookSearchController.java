54147cbbf4d9979c7b7feb3c41fdb82992c4b8e9
Jaeeon Lee <dannylee88@naver.com>
2023-06-24 11:26:51 +0900

    list page

--- src/main/java/com/example/booksearch/controller/BookSearchController.java
+++ src/main/java/com/example/booksearch/controller/BookSearchController.java
@@ -1,2 +1,17 @@
-package com.example.booksearch.controller;public class BookSearchController {
+package com.example.booksearch.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/booksearch")
+@Log4j2
+public class BookSearchController {
+
+    @GetMapping({"/", "/list"})
+    public String list() {
+        return "/booksearch/list";
+    }
 }
