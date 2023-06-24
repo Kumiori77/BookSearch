8322cdf1f069cd40baadcb1c8ccd62eea1484e21
Jaeeon Lee <dannylee88@naver.com>
2023-06-24 11:50:58 +0900

    QueryDsl setting

--- src/main/java/com/example/booksearch/repository/BookSearchRepository.java
+++ src/main/java/com/example/booksearch/repository/BookSearchRepository.java
@@ -2,6 +2,8 @@ package com.example.booksearch.repository;
 
 import com.example.booksearch.entity.BookSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface BookSearchRepository extends JpaRepository<BookSearch, Long> {
+public interface BookSearchRepository extends JpaRepository<BookSearch, Long>,
+        QuerydslPredicateExecutor<BookSearch> {
 }
